61479a3de9037999a06a57da1ee9fd98bacf716b
sena <sena@sena-MS-7817>
2017-04-19 08:48:08 -0500

    Tercero commit

--- JavaApplication2/src/NewMain.java
+++ JavaApplication2/src/NewMain.java
@@ -14,7 +14,7 @@ public class NewMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Hola Mundo");
     }
     
 }
