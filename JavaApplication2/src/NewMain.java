f08835d3803909459a5ca139b33b62ebb1da93e1
sena <sena@sena-MS-7817>
2017-04-25 09:27:27 -0500

    Sexto commit

--- JavaApplication2/src/NewMain.java
+++ JavaApplication2/src/NewMain.java
@@ -14,7 +14,7 @@ public class NewMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Diego Bien");
+        System.out.println("Hola Chicos");
     }
-    
+
 }
